4263fbe78ebfe4a843e1d6042f444ab7bb997d35
Daniel-Atanasov <daniel.a.atanasov97@gmail.com>
2018-02-10 23:52:57 +0200

    Added attribute implementation Added base rendering loop with callback(s)

--- src/Window.hpp
+++ src/Window.hpp
@@ -5,23 +5,43 @@
 #ifndef GLEE_WINDOW_HPP
 #define GLEE_WINDOW_HPP
 
+#include <SDL2/SDL.h>
+#include <functional>
+#include <set>
 #include <string>
 
-class Window {
-  public:
-	Window(int width, int height);
+namespace glee {
+    class Window {
+      private:
+        using RenderCallback = std::function<void(Uint32 delta)>;
 
-	int getWidth();
-	int getHeight();
+        SDL_Window *_sdlWindow;
+        std::set<RenderCallback> _renderCallbacks;
 
-	void setTitle(const std::string& title);
-	void setSize(int width, int height);
+        bool _running;
 
+        Uint32 _frameLength;
 
-	void close();
+        void callRenderCallbacks(Uint32 delta);
 
+      public:
+        Window(const std::string &title, int x, int y, int width, int height);
 
-};
+        void loop();
 
+        std::string getTitle();
+        void setTitle(const std::string &title);
 
-#endif //GLEE_WINDOW_HPP
+        void getSize(int &width, int &height);
+        void setSize(int width, int height);
+
+        Uint32 getFrameLength();
+        void setFrameLength(Uint32 newLength);
+
+        void addRenderCallback(RenderCallback callback);
+
+        void stop();
+        void close();
+    };
+} // namespace glee
+#endif // GLEE_WINDOW_HPP
