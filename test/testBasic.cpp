d6d06bf198025fb13f7d5c3db8e10dbf7883bfb0
braynstorm <glav0r3zzz4@gmail.com>
2018-02-11 01:20:31 +0200

    Added test line drawing

--- test/testBasic.cpp
+++ test/testBasic.cpp
@@ -1,13 +1,36 @@
 #include "../src/GLEE.hpp"
 
 using glee::Window;
+using glee::Line;
+using glee::LineBatch;
+
+using glm::vec2;
+using glee::Line;
+using glee::LineBatch;
+
+using glm::vec2;
 
 int main() {
-    Window window{"GLEE Test Window", 10, 10, 800, 600};
+    Window window{ "GLEE Test Window", 10, 10, 800, 600 };
 
-    window.addEventCallback([](Window &window, Uint32 delta, SDL_Event event) {
+    LineBatch batch;
+    auto line1 = Line{
+        vec2{ 0.0, 0.0 },
+        vec2{ 100.0, 100.0 }
+    }.setColors(
+        0xFFFFFFFF,
+        0xFF00FF00
+    );
+
+    batch.addLine(line1);
+
+    window.addEventCallback([](Window& window, Uint32 delta, SDL_Event event) {
         if (event.type == SDL_QUIT) window.stop();
         return false;
+    });
+
+    window.addRenderCallback([&batch](Window& window, float delta) {
+        batch.draw();
     });
 
     window.loop();
