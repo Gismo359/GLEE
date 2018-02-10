c1c8c628b3fb99767f237b065cc83c705aacf79e
Daniel <daniel@localhost.localdomain>
2018-02-11 01:15:50 +0200

    Fixed basic window to use new callback signatures

--- test/testBasic.cpp
+++ test/testBasic.cpp
@@ -3,6 +3,12 @@
 using glee::Window;
 
 int main() {
-	Window window{ "GLEE Test Window", 10, 10, 800, 600 };
-	window.loop();
+    Window window{"GLEE Test Window", 10, 10, 800, 600};
+
+    window.addEventCallback([](Window &window, Uint32 delta, SDL_Event event) {
+        if (event.type == SDL_QUIT) window.stop();
+        return false;
+    });
+
+    window.loop();
 };
