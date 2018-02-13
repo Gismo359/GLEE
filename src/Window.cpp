feadb115ecd7b9d24918726bc0d2c157ddd119c3
Daniel <daniel.a.atanasov97@gmail.com>
2018-02-13 06:02:31 +0200

    Supports multiple windows at once (rendering not working currently for more than one) Removed unneeded logic from class and moved to GLEE

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,6 +2,8 @@
 // Created by braynstorm on 2/10/18.
 //
 
+#include "GLEE.hpp"
+
 #include <GL/glew.h>
 #include <sstream>
 #include <fstream>
@@ -9,215 +11,61 @@
 #include "Log.hpp"
 
 namespace glee {
-    using string= std::string;
+    std::vector<Window *> Window::_windowTracker;
 
-    void logGlErrors() {
-        auto error = glGetError();
-        if (error) {
-            LOG(WARN, "GL_ERROR: %s", glewGetErrorString(error));
-        }
+    void Window::Render(uint32_t delta) {
+        for (auto &&window : _windowTracker) { window->render(delta); }
     }
 
-    std::string readAllLines(std::string path){
-        std::ifstream file(path);
-        std::string line;
-        std::ostringstream stream;
-        while(std::getline(file, line)){
-            stream << line << '\n';
-        }
-
-        return stream.str();
-    }
-
-    void logGlShaderErrors(GLuint shader) {
-        GLint len = 100;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-        std::vector<char> log(static_cast<unsigned long>(len), ' ');
-        glGetShaderInfoLog(shader, static_cast<GLsizei>(log.size()), &len, &log[0]);
-
-        if (log.size() > 0) {
-            auto infoLogString = string(std::begin(log), std::end(log));
-
-            LOG(ERROR, "Couldn't compile shaders. %s", infoLogString.c_str());
-        }
-    }
-
-    void Window::compileShaderProgram() {
-        _shaderProgram = glCreateProgram();
-        auto shaderVertexSource = readAllLines("../shader_v.glsl");
-        auto shaderFragmentSource = readAllLines("../shader_f.glsl");
-
-        auto shaderVertex = glCreateShader(GL_VERTEX_SHADER);
-        auto shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
-
-        auto src_vertex = shaderVertexSource.c_str();
-        auto src_vertex_size = static_cast<GLint>(shaderVertexSource.size());
-
-        auto src_fragment = shaderFragmentSource.c_str();
-        auto src_fragment_size = static_cast<GLint>(shaderFragmentSource.size());
-
-        glShaderSource(shaderVertex, 1, &src_vertex, &src_vertex_size);
-        glShaderSource(shaderFragment, 1, &src_fragment, &src_fragment_size);
-
-        glCompileShader(shaderVertex);
-        logGlErrors();
-        logGlShaderErrors(shaderVertex);
-        glCompileShader(shaderFragment);
-        logGlErrors();
-        logGlShaderErrors(shaderFragment);
-
-        glAttachShader(_shaderProgram, shaderVertex);
-        logGlErrors();
-        glAttachShader(_shaderProgram, shaderFragment);
-        logGlErrors();
-
-        glLinkProgram(_shaderProgram);
-        logGlErrors();
-
-        GLint isLinked = 0;
-        glGetProgramiv(_shaderProgram, GL_LINK_STATUS, &isLinked);
-        if (isLinked == GL_FALSE) {
-
-            GLint maxLength = 0;
-            glGetProgramiv(_shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
-
-            std::vector<GLchar> infoLog(static_cast<unsigned long>(maxLength));
-            glGetProgramInfoLog(_shaderProgram, maxLength, &maxLength, &infoLog[0]);
-
-            auto infoLogString = string(std::begin(infoLog), std::end(infoLog));
-            LOG(ERROR, "Couldn't link shaders. %s", infoLogString.c_str());
-
-            glDeleteProgram(_shaderProgram);
-        }
-
-
-        glValidateProgram(_shaderProgram);
-        logGlErrors();
-
-        GLint isValid = 0;
-        glGetProgramiv(_shaderProgram, GL_VALIDATE_STATUS, &isValid);
-        if (isValid == GL_FALSE) {
-            GLint maxLength = 0;
-            glGetProgramiv(_shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
-
-            std::vector<GLchar> infoLog(static_cast<unsigned long>(maxLength));
-            glGetProgramInfoLog(_shaderProgram, maxLength, &maxLength, &infoLog[0]);
-
-            auto infoLogString = string(std::begin(infoLog), std::end(infoLog));
-
-            LOG(ERROR, "Couldn't link shaders. %s", infoLogString.c_str());
-
-            glDeleteProgram(_shaderProgram);
-        }
-
-        glUseProgram(_shaderProgram);
-
-        _uniformProjectionMatrix = glGetUniformLocation(_shaderProgram, "projectionMatrix");
-        _uniformModelViewMatrix = glGetUniformLocation(_shaderProgram, "modelViewMatrix");
-
-        float matrixIdentity[]{
-            1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f
-        };
-
-        glUniformMatrix4fv(_uniformProjectionMatrix, 1, GL_FALSE, matrixIdentity);
-        logGlErrors();
-        glUniformMatrix4fv(_uniformModelViewMatrix, 1, GL_FALSE, matrixIdentity);
-        logGlErrors();
-
-        glBindFragDataLocation(_shaderProgram, GL_COLOR_ATTACHMENT0, "pixelColor");
-
-    }
-
-
-    void Window::callRenderCallbacks(Uint32 delta) {
-        for (auto&& function : _renderCallbacks) { function(*this, delta); }
-    }
-
-    void Window::callEventCallbacks(Uint32 delta, SDL_Event event) {
-        for (auto&& function : _eventCallbacks) {
-            // TODO: Possibly add a handled flag via a wrapper and make EventCallback return void?
-            if (function(*this, delta, event))
-                // TODO: Do something else once handled?
-                break;
-        }
-    }
-
-    Window::Window(const std::string& title, int x, int y, int width, int height) {
-        if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
-            LOG(ERROR, "SDL Failed to initialize!");
-            abort();
-        }
+    Window::Window(const std::string &title, int x, int y, int width, int height) {
 
         _sdlWindow = SDL_CreateWindow(title.c_str(), x, y, width, height, SDL_WINDOW_OPENGL);
-        auto context = SDL_GL_CreateContext(_sdlWindow);
+        if (!_sdlWindow) { LOG(ERROR, "Failed to initialize window: %s", SDL_GetError()); }
 
+        _sdlGlContext = SDL_GL_CreateContext(_sdlWindow);
+        if (!_sdlGlContext) { LOG(ERROR, "Failed to initialize OpenGL context: %s", SDL_GetError()); }
         glewExperimental = true;
         auto error = glewInit();
 
-        if (error) {
-            LOG(INFO, "%s", glewGetErrorString(error));
-        }
+        if (error) { LOG(INFO, "%s", glewGetErrorString(error)); }
 
-        compileShaderProgram();
-        LOG(INFO, "%s -> %s", glGetString(GL_VENDOR), glGetString(GL_VERSION));
-    }
+        SDL_GL_SwapWindow(this->_sdlWindow);
 
-    void Window::loop() {
-        _running = true;
-
-        SDL_GL_SwapWindow(_sdlWindow);
-        Uint32 lastTick = SDL_GetTicks();
-
-        while (_running) {
-            Uint32 currentTick = SDL_GetTicks();
-            Uint32 delta = currentTick - lastTick;
-
-            SDL_Event event;
-            while (SDL_PollEvent(&event)) {
-                // TODO: Filter or preprocess some events?
-                callEventCallbacks(delta, event);
-            }
-
-            // TODO: Comprimise for multiple missed frames
-            if (delta > getFrameLength()) {
-                // TODO: Check for double-buffering and clearing options
-
-                glUseProgram(_shaderProgram);
-                callRenderCallbacks(delta);
-
-                SDL_GL_SwapWindow(_sdlWindow);
-            }
-        }
+        _windowTracker.push_back(this);
     }
 
     // TODO: Maybe add separate wrappers for width/height
-    void Window::getSize(int& width, int& height) const { SDL_GetWindowSize(_sdlWindow, &width, &height); }
+    void Window::getSize(int &width, int &height) const { SDL_GetWindowSize(_sdlWindow, &width, &height); }
 
     void Window::setSize(int width, int height) { SDL_SetWindowSize(_sdlWindow, width, height); }
 
-    Uint32 Window::getFrameLength() const { return _frameLength; }
+    void Window::swapBuffer() { SDL_GL_SwapWindow(this->_sdlWindow); }
 
-    void Window::setFrameLength(Uint32 newLength) { _frameLength = newLength; }
+    void Window::addRenderHandler(RenderHandler callback, CallbackData data) { this->_renderCallbacks.push_back(std::make_pair(callback, data)); }
+
+    void Window::render(uint32_t delta) const {
+        for (auto &&pair : this->_renderCallbacks) { pair.first(delta, pair.second); }
+        SDL_GL_SwapWindow(this->_sdlWindow);
+    }
+
+    void Window::makeCurrentContext() const { SDL_GL_MakeCurrent(this->_sdlWindow, this->_sdlGlContext); }
 
     std::string Window::getTitle() const { return SDL_GetWindowTitle(_sdlWindow); }
 
-    void Window::setTitle(const std::string& title) { SDL_SetWindowTitle(_sdlWindow, title.c_str()); }
+    void Window::setTitle(const std::string &title) { SDL_SetWindowTitle(_sdlWindow, title.c_str()); }
 
-    void Window::addRenderCallback(Window::RenderCallback callback) { _renderCallbacks.push_back(callback); }
-
-    void Window::addEventCallback(Window::EventCallback callback) { _eventCallbacks.push_back(callback); }
-
-    void Window::stop() {
-        // TODO: Do render-specific cleanup if necessary
-        _running = false;
+    uint32_t Window::getWindowId() const {
+        uint32_t windowId = SDL_GetWindowID(this->_sdlWindow);
+        if (!windowId) { LOG(ERROR, "Enable to retrieve window ID: %s", SDL_GetError()); }
+        return windowId;
     }
 
-    void Window::close() {
-        stop();
-        SDL_DestroyWindow(_sdlWindow);
-        SDL_Quit();
-    }
+    bool Window::operator<(Window other) const { return this->_sdlWindow < other._sdlWindow; }
+
+    bool Window::operator>(Window other) const { return this->_sdlWindow > other._sdlWindow; }
+
+    bool Window::operator==(Window other) const { return this->_sdlWindow == other._sdlWindow; }
+
+    Window::~Window() { SDL_DestroyWindow(_sdlWindow); }
+
 } // namespace glee
