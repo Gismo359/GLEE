7311ad0dc6892ad1d3425a4ebc0d55def0b0f59a
Daniel <daniel.a.atanasov97@gmail.com>
2018-02-13 06:04:30 +0200

    Added initialization logic Added main loop and event and render calls to callbacks Moved temporary shader initialization to here

--- src/GLEE.hpp
+++ src/GLEE.hpp
@@ -5,8 +5,29 @@
 #ifndef GLEE_GLEE_HPP
 #define GLEE_GLEE_HPP
 
-
 #include "Window.hpp"
 #include "Drawing.hpp"
 
-#endif //GLEE_GLEE_HPP
+namespace glee {
+
+    int Init();
+
+    void AddEventCallback(EventHandler handler, CallbackData data);
+
+    void ClearRenderCallbacks(Window &window);
+
+    // TODO: Maybe add options when starting/stopping?
+    void StartLoop();
+    void StopLoop();
+
+    // TODO: Maybe add pausing logic for rendering?
+    // void pause();
+
+    // Temporary shader compilation
+    // TODO: Move to proper section
+    void compileShaderProgram();
+
+    void Quit();
+} // namespace glee
+
+#endif // GLEE_GLEE_HPP
